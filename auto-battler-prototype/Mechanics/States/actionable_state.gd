15dfee3cad671a20530cc8bde3e462651037e18c
Camjkz <camjkz@hotmail.com>
2026-01-02 09:10:17 +0800

    plugin update + initial balanced strategy

--- auto-battler-prototype/Mechanics/States/actionable_state.gd
+++ auto-battler-prototype/Mechanics/States/actionable_state.gd
@@ -3,8 +3,22 @@ class_name ActionableState
 
 func update(_delta: float):
 	var decision : String = ""
+	var chosenStrategy: GlobalValues.STRATEGY = owner.strategy
+
+	if chosenStrategy == GlobalValues.STRATEGY.AGGRESSIVE:
+		decision = aggressive_strategy_logic()
+	elif chosenStrategy == GlobalValues.STRATEGY.BALANCED:
+		decision = balanced_strategy_logic()
+		
+	if decision != "" && decision != self.name:
+		transition.emit(self, decision)
+
+
+func aggressive_strategy_logic() -> String:
+	var decision : String = ""
 	var canAttack = owner.check_can_attack()
 	var wantToAttack = owner.check_want_to_attack()
+	
 	if canAttack:
 		decision = "BaseAttack"
 	else:
@@ -18,6 +32,23 @@ func update(_delta: float):
 				decision = "Idle"
 			else:
 				decision = "MoveForward"
-		
-	if decision != "" && decision != self.name:
-		transition.emit(self, decision)
+	
+	return decision
+
+
+func balanced_strategy_logic() -> String:
+	var decision : String = ""
+	var canAttack = owner.check_can_attack()
+	#var wantToAttack = owner.check_want_to_attack()
+	
+	if canAttack:
+		decision = "BaseAttack"
+	else:
+		if owner.distance < owner.distanceThreshold:
+			decision = "MoveBackward"
+		elif owner.distance > owner.distanceThreshold:
+			decision = "MoveForward"
+		else:
+			decision = "Idle"
+	
+	return decision
