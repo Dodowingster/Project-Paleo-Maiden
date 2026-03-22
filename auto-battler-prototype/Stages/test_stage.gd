3f939997a884d562fe49be6e7daa652cee003030
Camjkz <camjkz@hotmail.com>
2026-03-22 08:59:08 +0800

    ui wip

--- auto-battler-prototype/Stages/test_stage.gd
+++ auto-battler-prototype/Stages/test_stage.gd
@@ -15,3 +15,12 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("pause_action"):
 		get_tree().paused = !get_tree().paused
+	
+	if Input.is_action_just_pressed("pause_menu"):
+		get_tree().paused = !get_tree().paused
+		%PauseMenu.visible = !%PauseMenu.visible
+
+
+func _on_resume_btn_pressed() -> void:
+	get_tree().paused = !get_tree().paused
+	%PauseMenu.visible = !%PauseMenu.visible
---
+++ auto-battler-prototype/UI/Menus/pause_menu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+@onready var resumeBtn : Button = %ResumeBtn
+@onready var endBattleBtn : Button = %EndBattleBtn
