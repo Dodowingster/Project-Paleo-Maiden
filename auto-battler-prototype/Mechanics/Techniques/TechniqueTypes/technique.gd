c36928a9626c6419271b3e60dcc46ce2e8a8606b
KowalskiYukio <dzackwan@gmail.com>
2026-04-06 19:16:20 +0800

    Add implementation of PositionTrigger - not sure if this can actually pass - also changed how triggers are initialized in techniques - centerPos is currently hardcoded as 400.0 but should probably be changed to dynamic but idk how to do that lol

--- auto-battler-prototype/Mechanics/Techniques/TechniqueTypes/technique.gd
+++ auto-battler-prototype/Mechanics/Techniques/TechniqueTypes/technique.gd
@@ -47,9 +47,10 @@ func setup_triggers_and_effects(character: Character) -> void:
 	print(" Trigger setup for " + techniqueName)
 
 	for trigger in triggers:
-		#if trigger is ActionGoalTrigger or trigger is HpTrigger or trigger is RangeTrigger \
-		#or trigger is OppStateTrigger:
+		# if trigger is ActionGoalTrigger or trigger is HpTrigger or trigger is RangeTrigger \
+		# or trigger is OppStateTrigger or trigger is PositionTrigger:
 		trigger.character = character
+		print("character side: " + str(trigger.character.get_side()))
 		print("  " + trigger.name + " trigger initialized")
 	
 	print(" Effect setup for " + techniqueName)
--- auto-battler-prototype/Mechanics/Techniques/Triggers/pos_trigger.gd
+++ auto-battler-prototype/Mechanics/Techniques/Triggers/pos_trigger.gd
@@ -1,7 +1,34 @@
 extends Trigger
 class_name PositionTrigger
+## Trigger class for checking against a set position range
 
+## The direction to check the position
+enum CheckDir {TO_CENTER, TO_CORNER}
+@export var checkDirection: CheckDir
+
+## The coordinate to check against (inclusive). Must be less than pos2.
+@export var posCheck: float = 150.0
+
+## The center of the stage
+@export var centerPos: float = 400.0
+
+## Reference to the character node.
 @onready var character : Character
 
 func check_condition() -> bool:
-	return character.currentActionGoal > character.actionGoalTotal
+	var side = character.get_side()
+	var charPos = character.global_position.x
+
+	if checkDirection == CheckDir.TO_CENTER:
+		if side > 0:
+			return charPos >= centerPos - posCheck
+		else:
+			return charPos <= centerPos + posCheck
+
+	elif checkDirection == CheckDir.TO_CORNER:
+		if side > 0:
+			return charPos <= centerPos - posCheck
+		else:
+			return charPos >= centerPos + posCheck
+
+	return false
