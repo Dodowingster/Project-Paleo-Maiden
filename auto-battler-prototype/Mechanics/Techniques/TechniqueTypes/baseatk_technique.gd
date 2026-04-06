b32fd473fdd81cabf3c1bd62c6946e0a2d45867c
Camjkz <camjkz@hotmail.com>
2026-04-06 17:07:26 +0800

    can now add hurtboxes to techniques

--- auto-battler-prototype/Mechanics/Techniques/TechniqueTypes/baseatk_technique.gd
+++ auto-battler-prototype/Mechanics/Techniques/TechniqueTypes/baseatk_technique.gd
@@ -3,7 +3,7 @@ extends Technique
 func _ready() -> void:
 	super()
 	techniqueName = "BASEATK"
-	animName = "baseatk"
+	animName = "baseattack"
 
 func execute_technique() -> void:
 	print(characterName + " used %s", techniqueName)
