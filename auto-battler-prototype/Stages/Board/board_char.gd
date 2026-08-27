fbddfd93b2c8786ff19a31c0386c0c553648c386
Camjkz <camjkz@hotmail.com>
2026-08-27 23:23:11 +0800

    node based movement wip

--- auto-battler-prototype/Stages/Board/board_char.gd
+++ auto-battler-prototype/Stages/Board/board_char.gd
@@ -1,12 +1,34 @@
 extends CharacterBody2D
 
+@export var initialNode : MapNode
+@onready var currentNode : MapNode
 
-func _process(delta: float) -> void:
+
+func _ready() -> void:
+	position = initialNode.position
+	currentNode = initialNode
+
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_left"):
-		position.x -= 64
+		#position.x -= 64
+		if currentNode.leftNode:
+			currentNode = currentNode.leftNode
+			position = currentNode.position
 	elif Input.is_action_just_pressed("ui_down"):
-		position.y += 64
+		#position.y += 64
+		if currentNode.downNode:
+			currentNode = currentNode.downNode
+			position = currentNode.position
 	elif Input.is_action_just_pressed("ui_right"):
-		position.x += 64
+		#position.x += 64
+		if currentNode.rightNode:
+			currentNode = currentNode.rightNode
+			position = currentNode.position
 	elif Input.is_action_just_pressed("ui_up"):
-		position.y -= 64
+		#position.y -= 64
+		if currentNode.upNode:
+			currentNode = currentNode.upNode
+			position = currentNode.position
+	
+	
---
+++ auto-battler-prototype/Stages/Board/map_node.gd
@@ -0,0 +1,7 @@
+class_name MapNode
+extends Node2D
+
+@export var leftNode : MapNode
+@export var upNode : MapNode
+@export var rightNode : MapNode
+@export var downNode : MapNode
