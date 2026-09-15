d845c89b98c0cc1af8e347953cca8e95af80425c
Camjkz <camjkz@hotmail.com>
2026-09-15 19:15:17 +0800

    stat display

--- auto-battler-prototype/UI/Menus/Reusables/char_display.gd
+++ auto-battler-prototype/UI/Menus/Reusables/char_display.gd
@@ -2,7 +2,17 @@ extends VBoxContainer
 class_name CharDisplay
 
 @onready var selectedCharLabel : Label = %SelectedCharLabel
+@onready var atkValueLabel : Label = %AtkValueLabel
+@onready var defValueLabel : Label = %DefValueLabel
+@onready var spdValueLabel : Label = %SpdValueLabel
+@onready var minStaValueLabel : Label = %MinStaValueLabel
+@onready var maxStaValueLabel : Label = %MaxStaValueLabel
 
 func _ready() -> void:
 	if CurrentRunData.char_data:
 		selectedCharLabel.text = CurrentRunData.char_data.characterName
+		atkValueLabel.text = str(CurrentRunData.char_data.atk)
+		defValueLabel.text = str(CurrentRunData.char_data.def)
+		spdValueLabel.text = str(CurrentRunData.char_data.spd)
+		minStaValueLabel.text = str(CurrentRunData.char_data.minSta)
+		maxStaValueLabel.text = str(CurrentRunData.char_data.maxSta)
